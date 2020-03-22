6b57c528e9a557b4f08b6ea71f6e96eae79a2174
Lejing Huang <ohaluminum@users.noreply.github.com>
2020-03-22 16:52:05 -0500

    Build printMenu() function each option.

--- src/ShoppingCartManager.java
+++ src/ShoppingCartManager.java
@@ -5,42 +5,93 @@ public class ShoppingCartManager
 {
     public static void printMenu(ShoppingCart cart)
     {
-        System.out.println("MENU");
-        System.out.println("a - Add item to cart");
-        System.out.println("d - Remove item from cart");
-        System.out.println("c - Change item quantity");
-        System.out.println("i - Output items' descriptions");
-        System.out.println("o - Output shopping cart");
-        System.out.println("q - Quit");
-        System.out.println("");
-        System.out.println("Choose an option:");
+        char userOption;
+        String tempName;
+        String tempDescription;
+        int tempPrice;
+        int tempQuantity;
+        char tempNewLine;
+        ItemToPurchase tempItem;
 
+        while (true)
+        {
+            System.out.println("MENU");
+            System.out.println("a - Add item to cart");
+            System.out.println("d - Remove item from cart");
+            System.out.println("c - Change item quantity");
+            System.out.println("i - Output items' descriptions");
+            System.out.println("o - Output shopping cart");
+            System.out.println("q - Quit");
+            System.out.println("");
+            System.out.println("Choose an option:");
 
+            userOption = scnr.nextChar();
+
+            if (userOption == 'q')
+            {
+                break;
+            }
+
+            switch (userOption)
+            {
+                case 'a':
+                    System.out.println("ADD ITEM TO CART");
+                    System.out.println("Enter the item name:");
+                    tempName = scnr.nextLine();
+
+                    System.out.println("Enter the item description:");
+                    tempDescription = scnr.nextLine();
+
+                    System.out.println("Enter the item price:");
+                    tempPrice = scnr.nextInt();
+
+                    System.out.println("Enter the item quantity:");
+                    tempQuantity = scnr.nextInt();
+
+                    tempNewLine = scnr.nextLine();
+
+                    tempItem = new ItemToPurchase(tempName, tempPrice, tempQuantity, tempDescription);
+
+                    cart.addItem(tempItem);
+
+                    break;
+
+                case 'd':
+                    System.out.println("REMOVE ITEM FROM CART");
+                    System.out.println("Enter name of item to remove:");
+
+                    tempName = scnr.nextLine();
+                    cart.removeItem(tempName);
+
+                    break;
+
+                case 'c':
+                    System.out.println("CHANGE ITEM QUANTITY");
+                    System.out.println("Enter the item name:");
+
+                    break;
+
+                case 'i':
+                    System.out.println("OUTPUT ITEMS' DESCRIPTIONS");
+                    cart.printDescription();
+                    break;
+
+                case 'o':
+                    System.out.println("OUTPUT SHOPPING CART");
+                    cart.printTotal();
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
-
-
-
-
-    /*
-     Implement the printMenu() method.
-     printMenu() has a ShoppingCart parameter,
-     and outputs a menu of options to manipulate the shopping cart.
-     Each option is represented by a single character.
-     Build and output the menu within the method.
-
-If the an invalid character is entered,
-continue to prompt for a valid choice.
-Hint: Implement Quit before implementing other options.
-Call printMenu() in the main() method.
-Continue to execute the menu until the user enters q to Quit. (3 pts)
-*/
-
 
     public static void main(String[] args)
     {
         Scanner scnr = new Scanner(System.in);
-        String userName = "";
-        String todayDate = "";
+        String userName;
+        String todayDate;
 
         System.out.println("Enter Customer's Name:");
         userName = scnr.nextLine();
@@ -53,7 +104,7 @@ Continue to execute the menu until the user enters q to Quit. (3 pts)
         System.out.println("Customer Name: " + cart.getCustomerName());
         System.out.println("Today's Date: " + cart.getDate());
 
-
+        printMenu(cart);
 
     }
 }
