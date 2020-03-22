397d4bcaea419c5c91878e407357a39b23ed829c
Lejing Huang <ohaluminum@users.noreply.github.com>
2020-03-22 15:47:51 -0500

    Build printMenu() function framework.

--- src/ShoppingCartManager.java
+++ src/ShoppingCartManager.java
@@ -3,6 +3,39 @@ import java.util.Scanner;
 
 public class ShoppingCartManager
 {
+    public static void printMenu(ShoppingCart cart)
+    {
+        System.out.println("MENU");
+        System.out.println("a - Add item to cart");
+        System.out.println("d - Remove item from cart");
+        System.out.println("c - Change item quantity");
+        System.out.println("i - Output items' descriptions");
+        System.out.println("o - Output shopping cart");
+        System.out.println("q - Quit");
+        System.out.println("");
+        System.out.println("Choose an option:");
+
+
+    }
+
+
+
+
+    /*
+     Implement the printMenu() method.
+     printMenu() has a ShoppingCart parameter,
+     and outputs a menu of options to manipulate the shopping cart.
+     Each option is represented by a single character.
+     Build and output the menu within the method.
+
+If the an invalid character is entered,
+continue to prompt for a valid choice.
+Hint: Implement Quit before implementing other options.
+Call printMenu() in the main() method.
+Continue to execute the menu until the user enters q to Quit. (3 pts)
+*/
+
+
     public static void main(String[] args)
     {
         Scanner scnr = new Scanner(System.in);
